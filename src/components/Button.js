7c5098a1b36974b6dd18bcd712f322a0b1270949
Brian Guterl <brianguterl99@gmail.com>
2021-03-29 12:21:21 -0400

    replacement for redux

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,15 +4,15 @@ import ColorContext from '../contexts/ColorContext';
 
 class Button extends Component {
 
-  renderSubmit(value) {
-    return value === 'english' ? 'Submit' : 'Voorleggen';
+  renderSubmit(language) {
+    return language === 'english' ? 'Submit' : 'Voorleggen';
   }
 
   renderButton(color) {
     return (
       <button className={`ui button ${color}`}>
         <LanguageContext.Consumer>
-          {(value) => this.renderSubmit(value)}
+          {(value) => this.renderSubmit(value.language)}
         </LanguageContext.Consumer>
       </button>
     )
